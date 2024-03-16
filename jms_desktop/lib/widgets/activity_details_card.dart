034313ce29bc60f7acdba93660ddf1c0a6e64520
Randil Hasanga <randil_2020690@fot.ruh.ac.lk>
2024-03-16 18:32:42 +0530

    UI improvements

--- jms_desktop/lib/widgets/activity_details_card.dart
+++ jms_desktop/lib/widgets/activity_details_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:jms_desktop/const/constants.dart';
 
 class ActivityDetailsCardWidget extends StatefulWidget {
   @override
@@ -11,7 +12,6 @@ class ActivityDetailsCardWidget extends StatefulWidget {
 
 class _ActivityDetailsCardState extends State<ActivityDetailsCardWidget> {
   double? _deviceWidth, _deviceHeight, _fontSize;
-  
 
   @override
   Widget build(BuildContext context) {
@@ -19,205 +19,233 @@ class _ActivityDetailsCardState extends State<ActivityDetailsCardWidget> {
     _deviceWidth = MediaQuery.of(context).size.width;
     _fontSize = _deviceHeight! * _deviceWidth! / 50000;
 
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Expanded(
-                flex: 2,
-                child: Text(
-                  "Total",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: _deviceWidth! * 0.02,
+    return Container(
+      margin: EdgeInsets.all(_deviceWidth! * 0.01),
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(225, 255, 255, 255),
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 5,
+            offset: Offset(0, 0),
+          ),
+        ],
+      ),
+      //color: Colors.transparent,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Text(
+                    "Total",
+                    style: TextStyle(
+                      color: const Color.fromARGB(255, 0, 0, 0),
+                      fontSize: _fontSize! * 0.7,
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                width: _deviceWidth! * 0.02,
-              ),
-              Expanded(
-                child: Text(
-                  "Pending",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: _deviceWidth! * 0.02,
-                  ),
+                SizedBox(
+                  width: _deviceWidth! * 0.02,
                 ),
-              )
-            ],
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                flex: 1,
-                child: AnimatedContainer(
-                  duration: Duration(milliseconds: 300),
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(55, 255, 255, 255),
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Colors.black12,
-                        blurRadius: 10,
-                        offset: Offset(0, 5),
-                      ),
-                    ],
+                Expanded(
+                  child: Text(
+                    "Pending",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: _fontSize! * 0.7,
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
-                  height: _deviceHeight! * 0.2,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.max,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                )
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: AnimatedContainer(
+                    duration: Duration(milliseconds: 300),
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(225, 255, 255, 255),
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 5,
+                          offset: Offset(0, 0),
+                        ),
+                      ],
+                    ),
+                    height: _deviceHeight! * 0.2,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: _deviceWidth! * 0.01,
+                          vertical: _deviceHeight! * 0.015),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
                         mainAxisSize: MainAxisSize.max,
-                        crossAxisAlignment: CrossAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
                             "Job Seekers ",
                             style: TextStyle(
-                              color: Colors.white,
-                              fontSize: _fontSize! * 1,
+                              color: Colors.black,
+                              fontSize: _fontSize! * 0.6,
                             ),
                           ),
-                          Text(
-                            // TODO: get data from DB
-                            "20",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: _fontSize! * 2,
-                            ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                // TODO: get data from DB
+                                "20",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: _fontSize! * 2,
+                                ),
+                              ),
+                            ],
                           ),
                         ],
                       ),
-                    ],
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                width: 20,
-              ),
-              Expanded(
-                flex: 1,
-                child: AnimatedContainer(
-                  duration: Duration(milliseconds: 300),
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(55, 255, 255, 255),
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Colors.black12,
-                        blurRadius: 10,
-                        offset: Offset(0, 5),
-                      ),
-                    ],
-                  ),
-                  height: _deviceHeight! * 0.2,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.max,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      // Icon(Icons.search,
-                      //     size: _deviceWidth! * 0.03, color: Colors.white),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                const SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                  flex: 1,
+                  child: AnimatedContainer(
+                    duration: Duration(milliseconds: 300),
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(225, 255, 255, 255),
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 5,
+                          offset: Offset(0, 0),
+                        ),
+                      ],
+                    ),
+                    height: _deviceHeight! * 0.2,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: _deviceWidth! * 0.01,
+                          vertical: _deviceHeight! * 0.015),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
                         mainAxisSize: MainAxisSize.max,
-                        crossAxisAlignment: CrossAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
                             "Job Providers ",
                             style: TextStyle(
-                              color: Colors.white,
-                              fontSize: _fontSize! * 1,
+                              color: Colors.black,
+                              fontSize: _fontSize! * 0.6,
                             ),
                           ),
-                          Text(
-                            // TODO: get data from DB
-                            "20",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: _fontSize! * 2,
-                            ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                // TODO: get data from DB
+                                "20",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: _fontSize! * 2,
+                                ),
+                              ),
+                            ],
                           ),
                         ],
                       ),
-                    ],
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                width: 20,
-              ),
-              Expanded(
-                flex: 1,
-                child: AnimatedContainer(
-                  duration: Duration(milliseconds: 300),
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(55, 255, 255, 255),
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Colors.black12,
-                        blurRadius: 10,
-                        offset: Offset(0, 5),
-                      ),
-                    ],
-                  ),
-                  height: _deviceHeight! * 0.2,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.max,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      // Icon(Icons.search,
-                      //     size: _deviceWidth! * 0.03, color: Colors.white),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                const SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                  flex: 1,
+                  child: AnimatedContainer(
+                    duration: Duration(milliseconds: 300),
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(225, 255, 255, 255),
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 5,
+                          offset: Offset(0, 0),
+                        ),
+                      ],
+                    ),
+                    height: _deviceHeight! * 0.2,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: _deviceWidth! * 0.01,
+                          vertical: _deviceHeight! * 0.015),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
                         mainAxisSize: MainAxisSize.max,
-                        crossAxisAlignment: CrossAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
                             "Job Providers ",
                             style: TextStyle(
-                              color: Colors.white,
-                              fontSize: _fontSize! * 1,
+                              color: Colors.black,
+                              fontSize: _fontSize! * 0.6,
                             ),
                           ),
-                          Text(
-                            // TODO: get data from DB
-                            "20",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: _fontSize! * 2,
-                            ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                // TODO: get data from DB
+                                "20",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: _fontSize! * 2,
+                                ),
+                              ),
+                            ],
                           ),
                         ],
                       ),
-                    ],
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+            SizedBox(
+              height: _deviceHeight! * 0.02,
+            ),
+            // Container(
+            //   height: 1.5,
+            //   color: Colors.grey,
+            // ),
+          ],
+        ),
       ),
     );
   }
-
 }
