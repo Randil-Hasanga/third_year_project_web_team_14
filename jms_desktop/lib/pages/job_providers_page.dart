43b902526ef79742009ce36cc9f60b4690ea5c40
Randil Hasanga <randil_2020690@fot.ruh.ac.lk>
2024-03-30 16:03:42 +0530

    Update job_providers_page.dart

--- jms_desktop/lib/pages/job_providers_page.dart
+++ jms_desktop/lib/pages/job_providers_page.dart
@@ -5,6 +5,8 @@ import 'package:jms_desktop/const/constants.dart';
 import 'package:jms_desktop/pages/test.dart';
 import 'package:jms_desktop/services/firebase_services.dart';
 
+double? _deviceWidth, _deviceHeight, _widthXheight;
+
 class JobProviders extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -15,8 +17,10 @@ class JobProviders extends StatefulWidget {
 class _JobProvidersState extends State<JobProviders> {
   FirebaseService? _firebaseService;
   List<Map<String, dynamic>>? jobProviders;
-  double? _deviceWidth, _deviceHeight, _widthXheight;
+
   ScrollController _scrollControllerLeft = ScrollController();
+  bool _isDetailsVisible = false;
+  Map<String, dynamic>? _selectedProvider;
 
   @override
   void initState() {
@@ -48,9 +52,11 @@ class _JobProvidersState extends State<JobProviders> {
             ),
             Expanded(
               flex: 2,
-              child: Container(
-                color: Colors.amber,
-              ),
+              child: _isDetailsVisible
+                  ? SelectedProviderDetailsWidget(_selectedProvider)
+                  : Container(
+                      color: Colors.amber,
+                    ),
             )
           ],
         ),
@@ -58,89 +64,93 @@ class _JobProvidersState extends State<JobProviders> {
     );
   }
 
- Widget CurrentProvidersListWidget() {
-  return Container(
-    margin: EdgeInsets.only(
-        left: _deviceWidth! * 0.01, bottom: _deviceHeight! * 0.02),
-    padding: EdgeInsets.symmetric(horizontal: _deviceWidth! * 0.01),
-    // height: _deviceHeight! * 0.27,
-    decoration: BoxDecoration(
-      //color: Color.fromARGB(172, 255, 255, 255),
-      color: cardBackgroundColorLayer2,
-      borderRadius: BorderRadius.circular(_widthXheight! * 1),
-      boxShadow: const [
-        BoxShadow(
-          color: Colors.black12,
-          blurRadius: 5,
-          offset: Offset(0, 0),
-        ),
-      ],
-    ),
-    child: Column(
-      children: [
-        Align(
-            alignment: Alignment.topLeft,
-            child: Padding(
-              padding: EdgeInsets.only(
-                  top: _widthXheight! * 0.7, left: _widthXheight! * 0.1),
-              child: Row(
-                children: [
-                  Text(
-                    "Current Job Providers",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: _widthXheight! * 0.7,
-                      fontWeight: FontWeight.w600,
+  Widget CurrentProvidersListWidget() {
+    return Container(
+      margin: EdgeInsets.only(
+        left: _deviceWidth! * 0.01,
+        bottom: _deviceHeight! * 0.02,
+        top: _deviceHeight! * 0.02,
+      ),
+      padding: EdgeInsets.symmetric(horizontal: _deviceWidth! * 0.01),
+      decoration: BoxDecoration(
+        color: cardBackgroundColorLayer2,
+        borderRadius: BorderRadius.circular(_widthXheight! * 1),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 5,
+            offset: Offset(0, 0),
+          ),
+        ],
+      ),
+      child: Column(
+        children: [
+          Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: EdgeInsets.only(
+                    top: _widthXheight! * 0.7, left: _widthXheight! * 0.1),
+                child: Row(
+                  children: [
+                    Text(
+                      "Current Job Providers",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: _widthXheight! * 0.7,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+              )),
+          Expanded(
+            child: Stack(
+              children: [
+                Visibility(
+                  visible: jobProviders == null,
+                  child: Center(
+                    child: CircularProgressIndicator(
+                      color: selectionColor,
                     ),
                   ),
-                ],
-              ),
-            )),
-        Expanded(
-          child: Stack(
-            children: [
-              Visibility(
-                visible: jobProviders == null,
-                child: const Center(
-                  child: CircularProgressIndicator(
-                    color: selectionColor,
-                  ),
                 ),
-              ),
-              Visibility(
-                visible: jobProviders != null,
-                child: Scrollbar(
-                  controller: _scrollControllerLeft,
-                  thumbVisibility: true,
-                  child: ListView.builder(
+                Visibility(
+                  visible: jobProviders != null,
+                  child: Scrollbar(
                     controller: _scrollControllerLeft,
-                    shrinkWrap: true,
-                    itemCount: jobProviders?.length ?? 0,
-                    scrollDirection: Axis.vertical,
-                    itemBuilder: (context, index) {
-                      return CurrentListViewBuilderWidget(index);
-                    },
+                    thumbVisibility: true,
+                    child: ListView.builder(
+                      controller: _scrollControllerLeft,
+                      shrinkWrap: true,
+                      itemCount: jobProviders?.length ?? 0,
+                      scrollDirection: Axis.vertical,
+                      itemBuilder: (context, index) {
+                        return CurrentListViewBuilderWidget(
+                            jobProviders![index]);
+                      },
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-      ],
-    ),
-  );
-}
+        ],
+      ),
+    );
+  }
 
-  Widget CurrentListViewBuilderWidget(int index) {
-    Map<String, dynamic> provider = jobProviders?[index] ?? {};
+  Widget CurrentListViewBuilderWidget(Map<String, dynamic> provider) {
     return Padding(
       padding: EdgeInsets.only(
         right: _deviceWidth! * 0.0125,
         top: _deviceHeight! * 0.01,
       ),
       child: GestureDetector(
-        onTap: (){
-
+        onTap: () {
+          setState(() {
+            _isDetailsVisible = true;
+            _selectedProvider = provider;
+          });
         },
         child: AnimatedContainer(
           duration: Duration(microseconds: 300),
@@ -185,197 +195,49 @@ class _JobProvidersState extends State<JobProviders> {
   }
 }
 
+class SelectedProviderDetailsWidget extends StatelessWidget {
+  final Map<String, dynamic>? provider;
 
+  const SelectedProviderDetailsWidget(this.provider);
 
-// import 'package:flutter/material.dart';
-// import 'package:jms_desktop/const/constants.dart';
-// class JobProviders extends StatefulWidget {
-//   @override
-//   State<StatefulWidget> createState() {
-//     return _JobProvidersState();
-//   }
-// }
-
-// class _JobProvidersState extends State<JobProviders> {
-//   JobProvider? selectedJobProvider;
-//   double? _deviceWidth, _deviceHeight, _widthXheight;
-//   ScrollController _scrollControllerLeft = ScrollController();
-
-//   onJobProviderTapped(JobProvider jobProvider) {
-//     setState(() {
-//       selectedJobProvider = jobProvider;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     _deviceHeight = MediaQuery.of(context).size.height;
-//     _deviceWidth = MediaQuery.of(context).size.width;
-//     _widthXheight = _deviceHeight! * _deviceWidth! / 50000;
-//     return Scaffold(
-//       body: SafeArea(
-//         child: Row(
-//           children: [
-//             Expanded(
-//               flex: 1,
-//               child: CurrentProvidersListWidget(
-//                 onTap: onJobProviderTapped,
-//               ),
-//             ),
-//             Expanded(
-//               flex: 2,
-//               child: selectedJobProvider == null
-//                   ? Center(
-//                       child: Text("No job provider selected"),
-//                     )
-//                   : JobProviderDetails(
-//                       jobProvider: selectedJobProvider!,
-//                     ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-
-//   Widget CurrentProvidersListWidget({required Function(JobProvider) onTap}) {
-//     return Container(
-//       margin: EdgeInsets.only(
-//           left: _deviceWidth! * 0.01, bottom: _deviceHeight! * 0.02),
-//       padding: EdgeInsets.symmetric(horizontal: _deviceWidth! * 0.01),
-//       // height: _deviceHeight! * 0.27,
-//       decoration: BoxDecoration(
-//         //color: Color.fromARGB(172, 255, 255, 255),
-//         color: cardBackgroundColorLayer2,
-//         borderRadius: BorderRadius.circular(_widthXheight! * 1),
-//         boxShadow: const [
-//           BoxShadow(
-//             color: Colors.black12,
-//             blurRadius: 5,
-//             offset: Offset(0, 0),
-//           ),
-//         ],
-//       ),
-//       child: Column(
-//         children: [
-//           Align(
-//               alignment: Alignment.topLeft,
-//               child: Padding(
-//                 padding: EdgeInsets.only(
-//                     top: _widthXheight! * 0.7, left: _widthXheight! * 0.1),
-//                 child: Row(
-//                   children: [
-//                     Text(
-//                       "Current Job Providers",
-//                       style: TextStyle(
-//                         color: Colors.black,
-//                         fontSize: _widthXheight! * 0.7,
-//                         fontWeight: FontWeight.w600,
-//                       ),
-//                     ),
-//                   ],
-//                 ),
-//               )),
-//           Expanded(
-//             child: Scrollbar(
-//               controller: _scrollControllerLeft,
-//               thumbVisibility: true,
-//               child: ListView.builder(
-//                 controller: _scrollControllerLeft,
-//                 shrinkWrap: true,
-//                 itemCount: 10,
-//                 scrollDirection: Axis.vertical,
-//                 itemBuilder: (context, index) {
-//                   return CurrentListViewBuilderWidget(
-//                     onTap: onTap,
-//                     jobProvider: JobProvider(name: "XYS"),
-//                   );
-//                 },
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-
-//   Widget CurrentListViewBuilderWidget({
-//     required Function(JobProvider) onTap,
-//     required JobProvider jobProvider,
-//   }) {
-//     return Padding(
-//       padding: EdgeInsets.only(
-//         right: _deviceWidth! * 0.0125,
-//         top: _deviceHeight! * 0.01,
-//       ),
-//       child: GestureDetector(
-//         onTap: () {
-//           onTap(jobProvider);
-//         },
-//         child: AnimatedContainer(
-//           duration: Duration(microseconds: 300),
-//           height: _deviceHeight! * 0.08,
-//           width: _deviceWidth! * 0.175,
-//           decoration: BoxDecoration(
-//             color: cardBackgroundColor,
-//             borderRadius: BorderRadius.circular(_widthXheight! * 0.66),
-//             boxShadow: const [
-//               BoxShadow(
-//                 color: Colors.black12,
-//                 blurRadius: 5,
-//                 offset: Offset(0, 0),
-//               ),
-//             ],
-//           ),
-//           child: Padding(
-//             padding: EdgeInsets.symmetric(
-//                 horizontal: _deviceWidth! * 0.001,
-//                 vertical: _deviceHeight! * 0.015),
-//             child: Row(
-//               mainAxisAlignment: MainAxisAlignment.start,
-//               mainAxisSize: MainAxisSize.max,
-//               crossAxisAlignment: CrossAxisAlignment.center,
-//               children: [
-//                 SizedBox(
-//                   width: _deviceWidth! * 0.01,
-//                 ),
-//                 Icon(
-//                   Icons.developer_mode,
-//                   size: _widthXheight! * 1,
-//                 ),
-//                 Text(jobProvider.name)
-//               ],
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// class JobProviderDetails extends StatelessWidget {
-//   final JobProvider jobProvider;
-
-//   JobProviderDetails({required this.jobProvider});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       child: Column(
-//         children: [
-//           Text(jobProvider.name),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-
-// class JobProvider {
-//   String name;
-//   // Add other job provider details here
-
-//   JobProvider({required this.name});
-
-//   // Add other job provider methods here
-// }
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    _widthXheight = _deviceHeight! * _deviceWidth! / 50000;
+    return Container(
+      margin: EdgeInsets.symmetric(
+          horizontal: _deviceWidth! * 0.01, vertical: _deviceHeight! * 0.02),
+      padding: EdgeInsets.symmetric(horizontal: _deviceWidth! * 0.01),
+      decoration: BoxDecoration(
+        color: cardBackgroundColorLayer2,
+        borderRadius: BorderRadius.circular(_widthXheight! * 1),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 5,
+            offset: Offset(0, 0),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Details of Selected Job Provider",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 20),
+          if (provider != null) ...{
+            Text("Name: ${provider!['fname']}"),
+            Text("Email: ${provider!['email']}"),
+            // Add more details as needed
+          }
+        ],
+      ),
+    );
+  }
+}
