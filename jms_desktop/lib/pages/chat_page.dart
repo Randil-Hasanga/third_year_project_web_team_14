79d562e4c81a8b5b29072d1343377c2bf5df72fe
Dinuka2K <dinuka_2020693@fot.ruh.ac.lk>
2024-07-07 15:40:34 +0530

    create two flex UI

--- jms_desktop/lib/pages/chat_page.dart
+++ jms_desktop/lib/pages/chat_page.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
-
-import '../services/firebase_services.dart';
-import '../widgets/Search_bar_widget.dart';
-import '../widgets/buttons.dart';
-import '../widgets/richText.dart';
+import 'package:jms_desktop/const/constants.dart';
+import 'package:jms_desktop/services/chat_services.dart';
+import 'package:jms_desktop/services/firebase_services.dart';
+import 'package:jms_desktop/widgets/richText.dart';
 
 double? _deviceWidth, _deviceHeight, _widthXheight;
 RichTextWidget? _richTextWidget;
-ButtonWidgets? _buttonWidgets;
-FirebaseService? _firebaseService;
 
 class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
@@ -19,32 +16,102 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
-  SearchBarWidget? _searchBarWidget;
-  bool _showLoader = true;
-  TextEditingController _searchController = TextEditingController();
-
-  ScrollController _scrollControllerLeft = ScrollController();
+  final ChatService _chatService = ChatService();
+  FirebaseService? _firebaseService;
+  List<Map<String, dynamic>>? user;
+  final ScrollController _scrollControllerLeft = ScrollController();
   bool _isDetailsVisible = false;
-
-  double? _userListWidth, _userListHeight, _chatWidthXheight;
-  bool _showNoUserFound = false;
+  Map<String, dynamic>? _selectedUser;
+  bool _showLoader = true;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     _firebaseService = GetIt.instance.get<FirebaseService>();
-    _searchBarWidget = GetIt.instance.get<SearchBarWidget>();
     _richTextWidget = GetIt.instance.get<RichTextWidget>();
-    _buttonWidgets = GetIt.instance.get<ButtonWidgets>();
+    //_loadUsers();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      if (mounted) {
+        setState(() {
+          _showLoader = false;
+        });
+      }
+    });
+  }
+
+  void _loadUsers() async {
+    List<Map<String, dynamic>> data =
+        _chatService.getUserStream() as List<Map<String, dynamic>>;
+    if (mounted) {
+      setState(() {
+        user = data;
+      });
+    }
+
+    print(data);
   }
 
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    _widthXheight = _deviceHeight! * _deviceWidth! / 50000;
     return Scaffold(
       body: SafeArea(
-        child: Row(),
+        child: Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: UserListWidget(),
+            ),
+            Expanded(
+              flex: 3,
+              child: _isDetailsVisible
+                  ? SelectedUserDetailsWidget(selectedUser: _selectedUser)
+                  : Container(
+                      child: Center(
+                        child: Text(
+                          "Select a user to open chat",
+                          style: TextStyle(
+                            fontSize: _widthXheight! * 0.5,
+                          ),
+                        ),
+                      ),
+                    ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  Widget UserListWidget() {
+    return Container(
+      margin: EdgeInsets.only(
+        left: _deviceWidth! * 0.01,
+        bottom: _deviceHeight! * 0.02,
+        top: _deviceHeight! * 0.02,
+      ),
+      padding: EdgeInsets.symmetric(horizontal: _deviceWidth! * 0.01),
+      decoration: BoxDecoration(
+        color: cardBackgroundColorLayer2,
+        borderRadius: BorderRadius.circular(_widthXheight! * 1),
+        boxShadow: const [
+          BoxShadow(color: Colors.black12, blurRadius: 5, offset: Offset(0, 0)),
+        ],
+      ),
+    );
+  }
+}
+
+class SelectedUserDetailsWidget extends StatelessWidget {
+  final Map<String, dynamic>? selectedUser;
+  const SelectedUserDetailsWidget({super.key, required this.selectedUser});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
